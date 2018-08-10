e847e0eb8e51c6173f302187c6a40b8f91bfc9c5
Juan Martinez <juamarti@microsoft.com>
2018-08-10 08:03:05 -0700

    Sync changes

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -1,15 +1,20 @@
 # thumbnail_maker.py
-import logging
-import os
 import time
-import threading
+import os
+import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+from queue import Queue
+from threading import Thread, Lock
+import multiprocessing
+import asyncio
+import aiohttp
+import aiofiles
 
 import PIL
 from PIL import Image
 
-FORMAT = "[%(threadName)s, %(asctime)s, %(levelname)s] %(message)s"
+FORMAT = "[%(process)d, %(threadName)s, %(asctime)s, %(levelname)s] %(message)s"
 logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format=FORMAT)
 
 
@@ -18,13 +23,43 @@ class ThumbnailMakerService(object):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
+        self.img_queue = multiprocessing.JoinableQueue()
+        self.dl_size = 0
+        self.resized_size = multiprocessing.Value('i', 0)
 
-    def download_image(self, url):
-        # download each image and save to the input dir
-        logging.info("downloading image at URL " + url)
+    async def download_image_coro(self, session, url):
         img_filename = urlparse(url).path.split('/')[-1]
-        urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-        logging.info("image saved to " + self.input_dir + os.path.sep + img_filename)
+        img_filepath = self.input_dir + os.path.sep + img_filename
+
+        logging.info("Beginning downloading image {}".format(img_filename))
+
+        # await asyncio.sleep(0.5)
+        async with session.get(url) as response:
+            async with aiofiles.open(img_filepath, mode='wb') as f:
+                content = await response.content.read()
+                await f.write(content)
+
+        self.dl_size += os.path.getsize(img_filepath)
+        self.img_queue.put(img_filename)
+        logging.info("Finished downloading image {}".format(img_filename))
+
+    # async def download_images_coro(self, img_url_list):
+    #     async with aiohttp.ClientSession() as session:
+    #         fs = []
+    #         for url in img_url_list:
+    #             fs.append(self.download_image_coro(session, url))
+    #         await asyncio.gather(*fs)
+
+    async def download_images_coro(self, img_url_list):
+        async with aiohttp.ClientSession() as session:
+            fs = []
+            for url in img_url_list:
+                asyncio.ensure_future(self.download_image_coro(session, url), loop=self._event_loop)
+
+    # async def download_images_coro(self, img_url_list):
+    #     async with aiohttp.ClientSession() as session:
+    #         for url in img_url_list:
+    #             await self.download_image_coro(session, url)
 
     def download_images(self, img_url_list):
         # validate inputs
@@ -33,58 +68,79 @@ class ThumbnailMakerService(object):
         os.makedirs(self.input_dir, exist_ok=True)
 
         logging.info("beginning image downloads")
-
         start = time.perf_counter()
-        threads = []
-        for url in img_url_list:
-            t = threading.Thread(target=self.download_image, args=(url,))
-            t.start()
-            threads.append(t)
 
-        for t in threads:
-            t.join()
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.download_images_coro(img_url_list))
+        finally:
+            loop.close()
 
         end = time.perf_counter()
-
         logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
 
     def perform_resizing(self):
         # validate inputs
-        if not os.listdir(self.input_dir):
-            return
         os.makedirs(self.output_dir, exist_ok=True)
 
         logging.info("beginning image resizing")
         target_sizes = [32, 64, 200]
-        num_images = len(os.listdir(self.input_dir))
 
-        start = time.perf_counter()
-        for filename in os.listdir(self.input_dir):
-            orig_img = Image.open(self.input_dir + os.path.sep + filename)
-            for basewidth in target_sizes:
-                img = orig_img
-                # calculate target height of the resized image to maintain the aspect ratio
-                wpercent = (basewidth / float(img.size[0]))
-                hsize = int((float(img.size[1]) * float(wpercent)))
-                # perform resizing
-                img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+        while True:
+            filename = self.img_queue.get()
+            if filename:
+                logging.info("resizing image {}".format(filename))
+                orig_img = Image.open(self.input_dir + os.path.sep + filename)
+                for basewidth in target_sizes:
+                    img = orig_img
+                    # calculate target height of the resized image to maintain the aspect ratio
+                    wpercent = (basewidth / float(img.size[0]))
+                    hsize = int((float(img.size[1]) * float(wpercent)))
+                    # perform resizing
+                    img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
 
-                # save the resized image to the output dir with a modified file name
-                new_filename = os.path.splitext(filename)[0] + \
-                    '_' + str(basewidth) + os.path.splitext(filename)[1]
-                img.save(self.output_dir + os.path.sep + new_filename)
+                    # save the resized image to the output dir with a modified file name
+                    new_filename = os.path.splitext(filename)[0] + \
+                        '_' + str(basewidth) + os.path.splitext(filename)[1]
+                    out_filepath = self.output_dir + os.path.sep + new_filename
+                    img.save(out_filepath)
 
-            os.remove(self.input_dir + os.path.sep + filename)
-        end = time.perf_counter()
+                    with self.resized_size.get_lock():
+                        self.resized_size.value += os.path.getsize(out_filepath)
 
-        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
+                os.remove(self.input_dir + os.path.sep + filename)
+                logging.info("done resizing image {}".format(filename))
+                self.img_queue.task_done()
+            else:
+                self.img_queue.task_done()
+                break
 
     def make_thumbnails(self, img_url_list):
         logging.info("START make_thumbnails")
         start = time.perf_counter()
 
+        num_processes = multiprocessing.cpu_count()
+        logging.info("Num processes: {}".format(num_processes))
+        procs = []
+        for _ in range(num_processes):
+            p = multiprocessing.Process(target=self.perform_resizing)
+            p.start()
+            procs.append(p)
+
         self.download_images(img_url_list)
-        # self.perform_resizing()
+
+        for _ in range(num_processes):
+            self.img_queue.put(None)
+
+        for p in procs:
+            p.join()
+
+        pending = asyncio.Task.all_tasks()
+        while(len(pending)):
+            self._event_loop.run_until_complete(asyncio.gather(*pending))
+            pending = [task for task in asyncio.Task.all_tasks() if not task.done()]
 
         end = time.perf_counter()
-        logging.info("END make_thumbnails in {} seconds\n\n".format(end - start))
+        logging.info("END make_thumbnails in {} seconds".format(end - start))
+        logging.info("Initial size of downloads: [{}]  Final size of images: [{}]\n\n".format(
+            self.dl_size, self.resized_size.value))
